bad41a5af86483a30878b78cd487fbccf3b5d9e2
Lars Ivar Hatledal <laht@ntnu.no>
2019-06-28 14:43:37 +0200

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@
 from conans import ConanFile, CMake
 
 
-class CsecorejniConan(ConanFile):
+class CseconsumerConan(ConanFile):
 
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
