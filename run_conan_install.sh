c1e0dde0e1b17801f027a01caec8b5983c98492e
Lars Ivar Hatledal <laht@ntnu.no>
2019-11-07 14:21:41 +0100

    Update to 0.6.0 add fmuproxy and remove debug build

--- run_conan_install.sh
+++ run_conan_install.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-conan remote add osp https://osp-conan.azurewebsites.net/artifactory/api/conan/conan-local --f
+export CONAN_REVISIONS_ENABLED="1"
+conan remote add osp https://osp-conan.azurewebsites.net/artifactory/api/conan/public --f
 conan user -p "Open Simulation Platform" -r osp osp
 
 unameOut="$(uname -s)"
 case "${unameOut}" in
-    Linux*)     conan install . -s build_type=Release -s compiler.libcxx=libstdc++11 --install-folder=cmake-build-release --build=missing
-                conan install . -s build_type=Debug -s compiler.libcxx=libstdc++11 --install-folder=cmake-build-debug --build=missing;;
-    MINGW*)     conan install . -s build_type=Debug --install-folder=cmake-build-debug --build=missing
-                conan install . -s build_type=Release --install-folder=cmake-build-release --build=missing;;
+    Linux*)     conan install . -s build_type=Release -s compiler.libcxx=libstdc++11 --install-folder=cmake-build-release --build=missing;;
+    MINGW*)     conan install . -s build_type=Release --install-folder=cmake-build-release --build=missing;;
 esac
